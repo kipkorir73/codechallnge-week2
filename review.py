082025e3f80135b9d05c5e9bc475ea08337e53b6
collins <kipkorirc583@gmail.com>
2023-08-29 12:00:57 +0300

    Correction on import

--- review.py
+++ review.py
@@ -1,32 +1,14 @@
-class Customer:
-  
+class Review:
+    all_reviews = []
 
-    def num_reviews(self):
-        return len(self.reviews)
+    def __init__(self, customer, restaurant, rating):
+        self.customer = customer
+        self.restaurant = restaurant
+        self.rating = rating
+        Review.all_reviews.append(self)
 
-    @classmethod
-    def find_by_name(cls, name):
-        for customer in cls.all_customers:
-            if customer.full_name() == name:
-                return customer
-        return None
+    def customer(self):
+        return self.customer
 
-    @classmethod
-    def find_all_by_given_name(cls, name):
-        matching_customers = []
-        for customer in cls.all_customers:
-            if customer.given_name == name:
-                matching_customers.append(customer)
-        return matching_customers
-
-
-class Restaurant:
-    
-
-    def average_star_rating(self):
-        total_ratings = sum(review.rating for review in self.reviews)
-        num_reviews = len(self.reviews)
-        if num_reviews > 0:
-            return total_ratings / num_reviews
-        else:
-            return 0
+    def restaurant(self):
+        return self.restaurant
